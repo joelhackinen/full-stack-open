70fb4a1451329dff650288246f883b5c9ce9f476
Joel <joel.hakkinen@aalto.fi>
2022-09-26 13:51:20 +0300

    3.19

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
-  const [message, setMessage] = useState('')
+  const [message, setMessageText] = useState('')
   const [messageColor, setMessageColor] = useState('')
 
   useEffect(() => {
@@ -21,14 +21,18 @@ const App = () => {
       })
   }, [])
 
+  const showMessage = (text, err) => {
+    setMessageText(text)
+    setMessageColor(err ? 'red' : 'green')
+    setTimeout(() => {
+      setMessageText('')
+    }, 5000)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.map(p => p.number).includes(newNumber)) {
-      setMessage(`${newNumber} is already added to phonebook`)
-      setMessageColor('red')
-      setTimeout(() => {
-        setMessage('')
-      }, 5000)
+      showMessage(`${newNumber} is already added to phonebook`, true)
     } else if (persons.map(p => p.name).includes(newName)) {
       if (window.confirm(`${newName} is already added, replace the old number with the new one?`)) {
         const person = persons.find(p => p.name === newName)
@@ -37,17 +41,12 @@ const App = () => {
           .update(person.id, changedPerson)
           .then(returnedPerson => {
             setPersons(persons.map(p => p.id !== returnedPerson.id ? p : returnedPerson))
-            setMessage(`Updated number for ${newName}`)
-            setMessageColor('green')
+            showMessage(`Updated number for ${newName}`, false)
           })
           .catch(error => {
             setPersons(persons.filter(p => p.id !== person.id))
-            setMessage(`${newName} has already been removed from the server`)
-            setMessageColor('red')
+            showMessage(`${newName} has already been removed from the server`, true)
           })
-        setTimeout(() => {
-          setMessage('')
-        }, 5000)
       }
     } else {
       const personObj = { name: newName, number: newNumber }
@@ -55,11 +54,10 @@ const App = () => {
         .create(personObj)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
-          setMessage(`Added ${newName}`)
-          setMessageColor('green')
-          setTimeout(() => {
-            setMessage('')
-          }, 5000)
+          showMessage(`Added ${newName}`, false)
+        })
+        .catch(error => {
+          showMessage(`${error.response.data}`, true)
         })
     }
     setNewName('')
@@ -70,12 +68,10 @@ const App = () => {
     if (window.confirm(`Delete ${person.name}?`)) {
       personService
         .remove(person.id)
-        .then(setPersons(persons.filter(p => p.id !== person.id)))
-      setMessage(`Deleted ${person.name}`)
-      setMessageColor('green')
-      setTimeout(() => {
-        setMessage('')
-      }, 5000)
+        .then(r => {
+          setPersons(persons.filter(p => p.id !== person.id))
+          showMessage(`Deleted ${person.name}`, false)
+        })
     }
   }
 
